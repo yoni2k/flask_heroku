1f761a006c1f62ac0103b6ae5c1a64fbae69dd98
Yoni Krichevsky <yoni.k81@gmail.com>
2020-05-19 21:44:07 +0300

    Deleting code for Aviad, cleaning up

--- flask_example.py
+++ flask_example.py
@@ -4,13 +4,7 @@ example_app = Flask(__name__)
 
 @example_app.route('/')
 def hello_world():
-    return 'Hello Full Stack Cohort Winter 2020. You are awesome! ' \
-           'This really works automatically!'
-
-
-@example_app.route('/aviad')
-def aviad():
-    return 'Aviad is the king of the world!'
+    return 'Hello Full Stack Cohort Winter 2020!'
 
 
 if __name__ == '__main__':
