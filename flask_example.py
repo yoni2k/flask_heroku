40030c01fd52c173cdc49a550edcf0a37e2f246c
Yoni Krichevsky <yoni.k81@gmail.com>
2020-05-19 21:12:10 +0300

    Initial version of flask example application

---
+++ flask_example.py
@@ -0,0 +1,11 @@
+from flask import Flask
+example_app = Flask(__name__)
+
+
+@example_app.route('/')
+def hello_world():
+    return 'Hello Full Stack Cohort Winter 2020'
+
+
+if __name__ == '__main__':
+    example_app.run()
