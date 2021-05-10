03bb816c5c4bebd1f2e0c6319ac6d2c2de140c7f
Yoni Krichevsky <yoni.k81@gmail.com>
2021-05-10 11:55:44 +0300

    Change text

--- flask_example.py
+++ flask_example.py
@@ -6,7 +6,7 @@ example_app = Flask(__name__)
 
 @example_app.route('/')
 def hello_world():
-    return 'Hello Fellows February 2020!'
+    return 'Hello Fellows February 2020. You rock!'
 
 
 if __name__ == '__main__':
